f5201035dbc61d2a039dbd63b39cbc86feb6d535
katampoi simon <katampoisimon@gmail.com>
2023-03-30 23:35:09 +0300

    changes made

---
+++ learning/src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+import "./index.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hello dev katampoi </h1>
+      <h2>feels good to be back </h2>
+    </div>
+  );
+}
