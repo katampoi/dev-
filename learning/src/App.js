1478aa75424123c3d5708f40ef7c842c66a48d39
katampoi simon <katampoisimon@gmail.com>
2023-04-03 18:43:33 +0300

    added search using filtered array

--- learning/src/App.js
+++ learning/src/App.js
@@ -8,6 +8,17 @@ import AppointmentInfo from "./components/AddAppointmentsInfo";
 function App() {
 
   let[appointmentList, setAppointmentList] = useState([]);
+  let[query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  )
 
   const fetchData = useCallback(()=>{
     fetchData(`/public/data.json`)
@@ -29,9 +40,11 @@ function App() {
         <BiCalendar className="inline-block text-red-400 align-top" /> Your
         Appointments
         <AddAppointments />
-        <Search />
+        <Search query={query} 
+        onQueryChange={myQuery => setQuery(myQuery)}
+        />
         <ul className="divide-y divide-gray-200">
-          {appointmentList.map((appointment) => (
+          {filteredAppointments.map((appointment) => (
             <AppointmentInfo key = {appointment.id}
              appointment={appointment}
              onDeleteAppointment={
--- learning/src/components/Search.js
+++ learning/src/components/Search.js
@@ -58,7 +58,7 @@ return (
 
 
 
-const Search = () =>{
+const Search = ({query, onQueryChange}) =>{
   let [ToggleSort, setToggleSort] = useState(false);
     return (
       <div className="py-5">
@@ -71,23 +71,28 @@ const Search = () =>{
             type="text"
             name="query"
             id="query"
-            value=""
-            
+            value={query}
+            onChange={(event) => {
+              onQueryChange(event.target.value);
+            }}
             className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 "
             placeholder="Search"
           />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <div>
               <button
-                type="button" onClick={()=> {setToggleSort(!ToggleSort)}}
+                type="button"
+                onClick={() => {
+                  setToggleSort(!ToggleSort);
+                }}
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
                 id="options-menu"
-                aria-haspopup  ="true"
+                aria-haspopup="true"
                 aria-expanded="true"
               >
                 Sort By <BiCaretDown className="ml-2" />
               </button>
-              <DropDown toggle = {ToggleSort} />
+              <DropDown toggle={ToggleSort} />
             </div>
           </div>
         </div>
